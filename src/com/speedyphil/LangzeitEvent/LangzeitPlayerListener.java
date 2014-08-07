9419d22ac198f58b75774cf40ea0ef892d9328fa
renickbuettner <renickbuettner@renicks-imac>
2014-08-07 15:52:08 +0200

    asd

--- src/com/speedyphil/LangzeitEvent/LangzeitPlayerListener.java
+++ src/com/speedyphil/LangzeitEvent/LangzeitPlayerListener.java
@@ -1,7 +1,32 @@
 package com.speedyphil.LangzeitEvent;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import com.speedyphil.Core.CreeperCantineShared;
 
 public class LangzeitPlayerListener implements Listener {
 
+	LangzeitManager lm = CreeperCantineShared.getLangzeitManager();
+
+	@EventHandler
+	public void onDeath(PlayerDeathEvent e){
+		
+		// Ob Spieler in LangzeitEvent Welt gestorben ist
+
+		Player p = (Player) e.getEntity();
+		
+		String world = p.getWorld().getName();
+		
+		if( world == lm.getWorldName()  ){
+		
+			
+			
+			
+		}
+		
+	}
+	
 }
