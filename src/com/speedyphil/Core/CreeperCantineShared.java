632d67a289b3dc762ef391431c6d1615d62e740b
philip260897 <philip260897@gmail.com>
2014-08-07 14:07:49 +0200

    Initial

---
+++ src/com/speedyphil/Core/CreeperCantineShared.java
@@ -0,0 +1,16 @@
+package com.speedyphil.Core;
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class CreeperCantineShared extends JavaPlugin
+{
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+}
