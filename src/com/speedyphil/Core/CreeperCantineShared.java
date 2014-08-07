0fa91d5d8e0ef45db872bba090c20f3807e5ca28
philip260897 <philip260897@gmail.com>
2014-08-07 15:00:26 +0200

    asdf

--- src/com/speedyphil/Core/CreeperCantineShared.java
+++ src/com/speedyphil/Core/CreeperCantineShared.java
@@ -4,9 +4,17 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.speedyphil.LangzeitEvent.LangzeitCommandListener;
+import com.speedyphil.LangzeitEvent.LangzeitConfiguration;
+import com.speedyphil.LangzeitEvent.LangzeitPlayerListener;
+
 
 public class CreeperCantineShared extends JavaPlugin
 {
+	private static LangzeitCommandListener lzCL = new LangzeitCommandListener();
+	private static LangzeitPlayerListener lzPL = new LangzeitPlayerListener();
+	private static LangzeitConfiguration lzCF = new LangzeitConfiguration();
+	
 	private static Plugin plugin;
 	private PluginManager pm;
 	
@@ -20,5 +28,9 @@ public class CreeperCantineShared extends JavaPlugin
 	public void onDisable()
 	{
 
+	}
+	
+	public static LangzeitConfiguration getLangzeitConfiguration() {
+		return lzCF;
 	}
 }
